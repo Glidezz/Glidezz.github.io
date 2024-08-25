924450cb75e86a393d8e21ec6b18d73fd0377a86
Eliaukz <U202115586@hust.edu.cn>
2024-08-25 18:52:29 +0800

    update

---
+++ public/CV/resume.md
@@ -0,0 +1,66 @@
+---
+title: Zida Yin
+---
+
+# Zida Yin
+
+
+## About
+
+
+A Little About Me
+:    Hello! My name is Zida Yin.
+:    I'm a Computer Science student in [hust](https://www.hust.edu.cn/)
+
+
+## Education
+
+
+2021 - current
+:   **华中科技大学, 计算机学院**
+  
+
+
+
+
+
+## Projects
+2023.10 - 2023.11
+:    **xv6**      
+:    xv6 是 MIT 开发的一个教学用的完整的类 Unix 操作系统，并且在 MIT 的操作系统课程 6.1810 中使用。[code](https://github.com/Eliaukz/xv6.git)
+
+
+2024.4 - 2024.5
+:    **6.824**     
+:    MIT 6.5840 是一门讲解分布式系统的课程。 我完成了mapreduce算法，raft算法的领导选举，日志复制及快照功能。并基于raft算法完成了一个多机k-v系统。[code](https://github.com/Eliaukz/6.5840.git)
+
+
+2024.2
+:    **minnow**     
+:    minnow 是计算机网络课程的一个经典实验，我实现了基于内存的字节流实现，tcp发送方和接收方，基础的arp协议和路由算法。[code](https://github.com/Eliaukz/minnow.git)
+
+hust课程设计
+:    1. 基于dpll算法的SAT求解器，并用于求解双数独。
+     2. 基于riscv指令集的五段流水线
+     3. riscv-pke：华科操作系统实验及课程设计
+
+## Skills
+
+2021 - Continuing
+:    * Languages: C, Python, Markdown, Cpp, Golang
+     * Operating Systems: Linux, Windows, macOS
+
+
+
+
+
+
+
+
+## Contact
+contact
+:   Github [https://github.com/Eliaukz](https://github.com/Eliaukz)
+:   Blog [https://eliaukz.github.io/](https://eliaukz.github.io/)
+:   email [eliaukz07@gmail.com]()
+
+
--- src/consts.ts
+++ src/consts.ts
@@ -18,12 +18,12 @@ type SocialLink = {
 }
 
 export const siteConfig: Config = {
-  title: "Astro Theme Cody",
+  title: "eliaukz",
   description: "",
   lang: "en-GB",
   profile: {
-    author: "Amy Dang",
-    description: "your bio description"
+    author: "eliaukz",
+    description: "hi everyone"
   }
 }
 
@@ -36,17 +36,17 @@ export const socialLinks: Array<SocialLink> = [
   {
     icon: "mdi:github",
     friendlyName: "Github",
-    link: "https://github.com/kirontoo/astro-theme-cody",
+    link: "https://github.com/eliaukz",
   },
-  {
-    icon: "mdi:linkedin",
-    friendlyName: "LinkedIn",
-    link: "#",
-  },
+  // {
+  //   icon: "mdi:linkedin",
+  //   friendlyName: "LinkedIn",
+  //   link: "#",
+  // },
   {
     icon: "mdi:email",
     friendlyName: "email",
-    link: "mailto:ndangamy@gmail.com",
+    link: "eliaukz07@gmail.com",
   },
   {
     icon: "mdi:rss",
@@ -68,9 +68,13 @@ export const NAV_LINKS: Array<{ title: string, path: string }> = [
     title: "Blog",
     path: "/blog",
   },
-  {
-    title: "Projects",
-    path: '/projects'
+  // {
+  //   title: "Projects",
+  //   path: '/projects'
+  // },
+   {
+    title: "CV",
+    path: '/CV/resume.html'
   },
   {
     title: "Archive",
